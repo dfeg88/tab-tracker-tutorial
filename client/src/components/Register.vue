b8d22c682eab1a53835720993d5d273097d1cc8f
Daniel Fegan <dfegan@protonmail.com>
2018-05-30 20:37:26 +0100

    front-end - install vuetify for material design,  WIP on register page back-end - initialise authentication policy middleware etc.

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,21 +1,41 @@
 <template>
-  <div>
-    <h1>Register</h1>
-    <input
-      type="email"
-      name="email"
-      v-model="email"
-      placeholder="email">
-
-    <input
-      type="password"
-      name="password"
-      v-model="password"
-      placeholder="password">
-
-    <button
-      @click="register">Register</button>
-  </div>
+  <v-app id="inspire">
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md4>
+            <v-card class="elevation-12">
+              <v-toolbar dark color="primary">
+                <v-toolbar-title>Register</v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    name="email"
+                    label="Email"
+                    type="email"
+                    v-model="email"
+                    :rules="[rules.required, rules.email]"
+                    required></v-text-field>
+                  <v-text-field
+                    id="password"
+                    name="password"
+                    label="Password"
+                    type="password"
+                    v-model="password"
+                    :rules="[rules.required, rules.password]"
+                    required></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn color="primary" @click="register">Register</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
@@ -24,21 +44,40 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: null,
+      rules: {
+        required: (value) => !!value || 'Required.',
+        password: (value) => {
+          const pattern = /^[a-zA-Z0-9]{8,32}$/
+          return pattern.test(value) || 'Invalid password'
+        },
+        email: (value) => {
+          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          return pattern.test(value) || 'Invalid e-mail.'
+        }
+      }
     }
   },
   methods: {
     async register () {
-      await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+        console.log(error.response.data.error)
+      }
     }
+  },
+  props: {
+    source: String
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
